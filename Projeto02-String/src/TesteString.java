788b0f9f1ef62425631cc3dad8000ad4dac9c468
Moisés Silva <moises.silva_ads@outlook.com>
2019-05-24 17:12:49 -0300

    SubStrings

---
+++ Projeto02-String/src/TesteString.java
@@ -0,0 +1,14 @@
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		String nome = "Ricardo";
+		nome += " da Silva";
+		nome += " Almeida";
+		nome = nome + " Alencar";
+		
+		System.out.println(nome);
+	}
+
+}
